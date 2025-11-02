5b9940e297725fdcd3adc00ba82ff21d8821ce3e
TAHAREZAEI <138865337+TAHAREZAEI@users.noreply.github.com>
2025-11-02 22:44:51 +0330

    code2 commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h2>پنل مدیریت</h2>
+      <ul>
+        <li>🏠 خانه</li>
+        <li>👤 کاربران</li>
+        <li>📊 گزارش‌ها</li>
+        <li>⚙️ تنظیمات</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
