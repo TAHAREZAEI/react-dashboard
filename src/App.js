48cb6260ef0ce1fb570c64e8c7b2a07f5862e508
TAHAREZAEI <138865337+TAHAREZAEI@users.noreply.github.com>
2025-11-02 22:58:04 +0330

    code commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
-import DashboardPage from "./pages/DashboardPage";
-import UsersPage from "./pages/UsersPage";
+import DashboardPage from "./Pages/DashboardPage";
+import UsersPage from "./Pages/UsersPage";
 import "./App.css";
 
 function App() {
