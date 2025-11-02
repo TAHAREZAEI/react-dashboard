a5ee56bc4c0fa046de912fd571db1da13bdd0014
TAHAREZAEI <138865337+TAHAREZAEI@users.noreply.github.com>
2025-11-02 22:43:24 +0330

    code commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import "./App.css";
 
+// کامپوننت اصلی پروژه
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dashboard">
+      <Sidebar />
+      <main className="main">
+        <Header />
+        <Dashboard />
+      </main>
     </div>
   );
 }
